acb51d081eab7fb93480d3f2f762572527cd91f6
Radu Gheorghescu <radugamingtv@gmail.com>
2017-11-26 23:10:11 +0200

    CORS SUPPORT

--- src/main/java/ro/pub/lmn/controller/api/HistoryApiController.java
+++ src/main/java/ro/pub/lmn/controller/api/HistoryApiController.java
@@ -1,10 +1,7 @@
 package ro.pub.lmn.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ro.pub.lmn.controller.HistoryController;
 import ro.pub.lmn.entity.dto.HistoryDTO;
 import ro.pub.lmn.service.HistoryService;
@@ -22,6 +19,7 @@ public class HistoryApiController {
         this.historyService = historyService;
     }
 
+    @CrossOrigin
     @GetMapping("")
     public List<HistoryDTO> getAllHistory(@RequestParam String lang){
         return historyService.findAll(lang);
